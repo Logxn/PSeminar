d3d2de5ee286e959a2e2aed70223032ba43de630
Logan <hello@loganthompson.de>
2019-05-18 20:34:10 +0200

    Cleanup
    * Adjusted AssemblyInfo
    * Removed Readme

--- PSeminar/Properties/AssemblyInfo.cs
+++ PSeminar/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("PSeminar")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("P-Seminar: Wanderweg Ickelheim - github.com/Logxn/ - www.loganthompson.de")]
+[assembly: AssemblyDescription("Darstellung des Tracks rund um Ickelheim")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.loganthompson.de")]
 [assembly: AssemblyProduct("PSeminar")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
